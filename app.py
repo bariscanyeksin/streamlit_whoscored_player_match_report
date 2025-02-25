25f44eff8779d736252451f8364ca85e21d45c97
bariscanyeksin <82212277+bariscanyeksin@users.noreply.github.com>
2025-02-25 14:54:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -64,20 +64,21 @@ def load_match_data(whoscored_match_id):
         )
         # Daha fazla header ekleyelim
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': '1',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'none',
-            'Sec-Fetch-User': '?1',
-            'Cache-Control': 'max-age=0',
-            'DNT': '1',
-            'Referer': 'https://www.google.com/',
-            'Upgrade-Insecure-Requests': '1'
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+            'cache-control': 'no-cache',
+            'pragma': 'no-cache',
+            'priority': 'u=0, i',
+            'sec-ch-ua': '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
+            'cookie': '__cf_bm=e8.qs5ik_oMz_qhGN7Emv_qbf0Ygaq1P81V1T0cgEBE-1740483891-1.0.1.1-jbyb_yUZauRudDdYRTV2ctRJduJ1TLz82YpzXEYjVx3V7xQixxq7Ue6NS9NSOd.iYHaMw4mjGLS2IaLlaY.r6CAg2KHQe2tG8RrqebLkkRE',
         }
 
         # Daha uzun timeout süresi ve retry mekanizması ekleyelim
