abd43965cd111c8427b03e33c25659af253ae1a1
bariscanyeksin <82212277+bariscanyeksin@users.noreply.github.com>
2025-03-04 19:50:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ st.set_page_config(
     initial_sidebar_state="expanded"  # Sidebar'ı otomatik açık başlat
 )
 
+os.system("playwright install")
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Poppins fontunu yükleme
