17604a895eaa6cfa53429e8f3c9371ffc1102aec
bariscanyeksin <82212277+bariscanyeksin@users.noreply.github.com>
2026-02-18 13:51:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -190,168 +190,164 @@ if whoscored_match_id!="" and fotmob_match_id!="":
         # İlk oyuncuyu varsayılan olarak seç
         with st.sidebar:
             selected_fotmob_player_name, selected_fotmob_player_id = player_select_container.selectbox("Select Fotmob Player", player_options, format_func=lambda x: x[0], index=0)
-            # Fotmob verilerini tekrar çek
-        fotmobData = fetch_fotmob_data(fotmob_match_id)
         
-        if fotmobData:
-            fotmobPlayerStats = fotmobData['content']['playerStats']
-            fotmobPlayerDf =  fotmobPlayerStats[str(selected_fotmob_player_id)]
-            player_name = fotmobPlayerDf['name']
-            try:
-                minutes = fotmobPlayerDf['stats'][0]['stats']['Minutes played']['stat']['value']
-            except (KeyError, IndexError, TypeError):
-                minutes = "-"
-            shots_data = fotmobData['content']['shotmap']['shots']
-            
-            def get_team_id_by_player_id(lineup_data, player_id):
-                for team_key in ["homeTeam", "awayTeam"]:
-                    if team_key in lineup_data:
-                        team = lineup_data[team_key]
-                        all_players = team.get("starters", []) + team.get("substitutes", [])
-                        for player in all_players:
-                            if int(player['id']) == int(player_id):
-                                return team['id']
-                return None  # Oyuncu bulunamazsa None döndür
-            
-            lineup_data = fotmobData['content']['lineup']
-            team_id = get_team_id_by_player_id(lineup_data, selected_fotmob_player_id)
-            
-            if team_id is not None:
-                fotmobTeamData = fetch_fotmob_team_data(team_id)
-                team_name = fotmobTeamData['details']['name']
-                teamColor1 = fotmobTeamData['overview']['teamColors']['lightMode']
-                teamColor2 = 'white'  #fotmobTeamData['overview']['teamColors']['darkMode']
+        fotmobPlayerStats = fotmobData['content']['playerStats']
+        fotmobPlayerDf =  fotmobPlayerStats[str(selected_fotmob_player_id)]
+        player_name = fotmobPlayerDf['name']
+        try:
+            minutes = fotmobPlayerDf['stats'][0]['stats']['Minutes played']['stat']['value']
+        except (KeyError, IndexError, TypeError):
+            minutes = "-"
+        shots_data = fotmobData['content']['shotmap']['shots']
+        
+        def get_team_id_by_player_id(lineup_data, player_id):
+            for team_key in ["homeTeam", "awayTeam"]:
+                if team_key in lineup_data:
+                    team = lineup_data[team_key]
+                    all_players = team.get("starters", []) + team.get("substitutes", [])
+                    for player in all_players:
+                        if int(player['id']) == int(player_id):
+                            return team['id']
+            return None  # Oyuncu bulunamazsa None döndür
+        
+        lineup_data = fotmobData['content']['lineup']
+        team_id = get_team_id_by_player_id(lineup_data, selected_fotmob_player_id)
+        
+        if team_id is not None:
+            fotmobTeamData = fetch_fotmob_team_data(team_id)
+            team_name = fotmobTeamData['details']['name']
+            teamColor1 = fotmobTeamData['overview']['teamColors']['lightMode']
+            teamColor2 = 'white'  #fotmobTeamData['overview']['teamColors']['darkMode']
 
-                general_data = fotmobData['general']
-                week = general_data['matchRound']
-                matchDay = general_data['matchTimeUTCDate']
-                parsed_date = datetime.fromisoformat(matchDay[:-1])
-                formatted_date = parsed_date.strftime("%d.%m.%Y")
-                leagueName = general_data['leagueName']
-                leagueId = general_data['parentLeagueId']
-                leagueSeason = "2025/2026"
-                leagueString = f"{leagueName} - {leagueSeason}"
-                if "/" in week:
-                    weekString = f"{week}  |  {formatted_date}"
-                else:
-                    weekString = f"{week}. Hafta  |  {formatted_date}"
-                homeTeamName = general_data['homeTeam']['name']
-                homeTeamId = general_data['homeTeam']['id']
-                awayTeamName = general_data['awayTeam']['name']
-                awayTeamId = general_data['awayTeam']['id']
-                result = fotmobData['header']['status']['scoreStr']
-                result_string = f"{homeTeamName} {result} {awayTeamName}"
-                matchDetailString = f"{leagueString}  |  {weekString}"
+            general_data = fotmobData['general']
+            week = general_data['matchRound']
+            matchDay = general_data['matchTimeUTCDate']
+            parsed_date = datetime.fromisoformat(matchDay[:-1])
+            formatted_date = parsed_date.strftime("%d.%m.%Y")
+            leagueName = general_data['leagueName']
+            leagueId = general_data['parentLeagueId']
+            leagueSeason = "2025/2026"
+            leagueString = f"{leagueName} - {leagueSeason}"
+            if "/" in week:
+                weekString = f"{week}  |  {formatted_date}"
+            else:
+                weekString = f"{week}. Hafta  |  {formatted_date}"
+            homeTeamName = general_data['homeTeam']['name']
+            homeTeamId = general_data['homeTeam']['id']
+            awayTeamName = general_data['awayTeam']['name']
+            awayTeamId = general_data['awayTeam']['id']
+            result = fotmobData['header']['status']['scoreStr']
+            result_string = f"{homeTeamName} {result} {awayTeamName}"
+            matchDetailString = f"{leagueString}  |  {weekString}"
 
-                team_logo_image_url = f"https://images.fotmob.com/image_resources/logo/teamlogo/{team_id}.png"
+            team_logo_image_url = f"https://images.fotmob.com/image_resources/logo/teamlogo/{team_id}.png"
 
-                # Ana uygulama
-                st.title("Match Analysis")
+            # Ana uygulama
+            st.title("Match Analysis")
 
-                # Renk tanımlamaları
-                pitch_color = '#d6c39f'
-                line_color = '#0e1117'
-                second_line_color = '#38435c'
-                green = '#1e7818'
-                orange = '#ff5d44'
-                blue = '#3572A5'
-                red = '#ad1e11'
-                yellow = '#c2a51d'
-                dark_yellow = '#8a7512'
-                gray = '#808080'
-                dark_gray = '#626262'
-                purple = '#3a1878'
-                transparent_color = '#FFFFFF00'
-                white_blue = '#c7ebf0'
-                white_green = '#c7f0dd'
-                heatmap_orange = '#f57c00'
+            # Renk tanımlamaları
+            pitch_color = '#d6c39f'
+            line_color = '#0e1117'
+            second_line_color = '#38435c'
+            green = '#1e7818'
+            orange = '#ff5d44'
+            blue = '#3572A5'
+            red = '#ad1e11'
+            yellow = '#c2a51d'
+            dark_yellow = '#8a7512'
+            gray = '#808080'
+            dark_gray = '#626262'
+            purple = '#3a1878'
+            transparent_color = '#FFFFFF00'
+            white_blue = '#c7ebf0'
+            white_green = '#c7f0dd'
+            heatmap_orange = '#f57c00'
 
-                if selected_player_option:
-                    # Oyuncu bilgilerini al
-                    selected_player_name, selected_player_id, selected_team_id, selected_team_name, selected_player_number = list_players_and_get_selection_from_df(
-                        team_players, 
-                        teams_dict, 
-                        selected_player_option, 
-                        selected_team_id
-                    )
-                    
-                    with st.sidebar:
-                        player_fontsize = st.slider("Player Font Size", 10, 35, 25)
-                    
-                    # Hata kontrolü
-                    if selected_player_id is not None and selected_team_id is not None and selected_fotmob_player_name is not None:
+            if selected_player_option:
+                # Oyuncu bilgilerini al
+                selected_player_name, selected_player_id, selected_team_id, selected_team_name, selected_player_number = list_players_and_get_selection_from_df(
+                    team_players, 
+                    teams_dict, 
+                    selected_player_option, 
+                    selected_team_id
+                )
+                
+                with st.sidebar:
+                    player_fontsize = st.slider("Player Font Size", 10, 35, 25)
+                
+                # Hata kontrolü
+                if selected_player_id is not None and selected_team_id is not None and selected_fotmob_player_name is not None:
+                    # Görselleştirmeleri oluştur
+                    def generate_visualizations():
                         # Görselleştirmeleri oluştur
-                        def generate_visualizations():
-                            # Görselleştirmeleri oluştur
-                            fig, axs = plt.subplots(2, 3, figsize=(20, 11), facecolor=pitch_color, edgecolor=line_color)
-                            
-                            # Grafiklerin her biri için uygun boyut ve font ayarları
-                            match_details(axs[0, 0], selected_fotmob_player_id, team_name, player_name, selected_player_number, minutes, teamColor1, teamColor2, player_fontsize)
-
-                            # df'nin boş olup olmadığını kontrol et
-                            if df.empty:
-                                axs[0, 1].text(0.5, 0.5, 'No data available for this player.', fontsize=12, ha='center', va='center')
-                            else:
-                                passes_and_key_passes(axs[0, 1], selected_player_id, df)
-                            
-                            touches_and_heatmap(axs[0, 2], selected_player_id, df)
-                            defensive_actions(axs[1, 0], selected_player_id, df)
-                            dribbling_carrying(axs[1, 1], selected_player_id, df)
-                            shotmap(axs[1, 2], selected_player_id, df, shots_data, selected_fotmob_player_id)
-                                                        
-                            # Yazı boyutları için genel ayarlar
-                            for ax in axs.flat:
-                                ax.title.set_fontsize(18)  # Başlık yazı boyutu
-                                ax.xaxis.label.set_fontsize(14)  # X ekseni etiket yazı boyutu
-                                ax.yaxis.label.set_fontsize(14)  # Y ekseni etiket yazı boyutu
-                                
-                            # Add text and images to the figure
-                            fig.text(0.2, 0.96, result_string, fontsize=34, fontweight='bold', ha='left', va='center', fontproperties=bold_prop, color=line_color)
-                            fig.text(0.2, 0.92, matchDetailString, fontsize=18, ha='left', va='center', fontproperties=prop, color=line_color)
-                            
-                            league_logo_url = f"https://images.fotmob.com/image_resources/logo/leaguelogo/{leagueId}.png"
-                            league_logo = Image.open(BytesIO(requests.get(league_logo_url).content)).convert("RGBA")
-
-                            add_image(league_logo, fig, left=0.12, bottom=0.9, width=0.075, height=0.10)
-                            
-                            # Atak yönü okunu eklemek
-                            ax = fig.add_subplot(111)  # Ana figüre eksen ekleyelim
-                            ax.set_axis_off()  # Eksenleri gizleyelim
-
-                            # FancyArrowPatch ile ok oluşturma
-                            arrow = FancyArrowPatch((0.8075, 0.015), (0.91, 0.015), mutation_scale=20, color=line_color, lw=2.5, arrowstyle='->', transform=ax.transAxes)
-                            ax.add_patch(arrow)  # Oku figüre ekle
-                            
-                            # Atak yönü yazısını eklemek
-                            fig.text(0.788, 0.1, 'Atak Yönü', fontsize=15, ha='center', va='center', fontproperties=prop, color=line_color)
-                            
-                            endnote_ax = plt.axes([0.8, 0.93, 0.1, 0.05])
-                            endnote_ax.text(0.98,0.5,'Veri: WhoScored ve FotMob\n@bariscanyeksin', color=line_color, fontsize=14, ha='right', va='center', fontproperties=prop, transform=endnote_ax.transAxes)
-                            endnote_ax.axis('off')
-                                                    
-                            # Görselleştirmeleri göster
-                            st.pyplot(fig)
-                            
-                            # Download button
-                            buf = io.BytesIO()
-                            fig.savefig(buf, format="png", dpi = 300, bbox_inches = "tight")
-                            buf.seek(0)
-                            
-                            st.download_button(
-                                label="Grafiği İndir",
-                                data=buf,
-                                file_name=f"{player_name}-{team_name}-{leagueName}-{weekString}.png",
-                                mime="image/png"
-                            )
+                        fig, axs = plt.subplots(2, 3, figsize=(20, 11), facecolor=pitch_color, edgecolor=line_color)
                         
-                        # Görselleştirmeleri oluştur
-                        generate_visualizations()
-                    else:
-                        st.error("Seçilen oyuncunun bilgileri alınamadı. Lütfen tekrar deneyin.")
+                        # Grafiklerin her biri için uygun boyut ve font ayarları
+                        match_details(axs[0, 0], selected_fotmob_player_id, team_name, player_name, selected_player_number, minutes, teamColor1, teamColor2, player_fontsize)
+
+                        # df'nin boş olup olmadığını kontrol et
+                        if df.empty:
+                            axs[0, 1].text(0.5, 0.5, 'No data available for this player.', fontsize=12, ha='center', va='center')
+                        else:
+                            passes_and_key_passes(axs[0, 1], selected_player_id, df)
+                        
+                        touches_and_heatmap(axs[0, 2], selected_player_id, df)
+                        defensive_actions(axs[1, 0], selected_player_id, df)
+                        dribbling_carrying(axs[1, 1], selected_player_id, df)
+                        shotmap(axs[1, 2], selected_player_id, df, shots_data, selected_fotmob_player_id)
+                                                    
+                        # Yazı boyutları için genel ayarlar
+                        for ax in axs.flat:
+                            ax.title.set_fontsize(18)  # Başlık yazı boyutu
+                            ax.xaxis.label.set_fontsize(14)  # X ekseni etiket yazı boyutu
+                            ax.yaxis.label.set_fontsize(14)  # Y ekseni etiket yazı boyutu
+                            
+                        # Add text and images to the figure
+                        fig.text(0.2, 0.96, result_string, fontsize=34, fontweight='bold', ha='left', va='center', fontproperties=bold_prop, color=line_color)
+                        fig.text(0.2, 0.92, matchDetailString, fontsize=18, ha='left', va='center', fontproperties=prop, color=line_color)
+                        
+                        league_logo_url = f"https://images.fotmob.com/image_resources/logo/leaguelogo/{leagueId}.png"
+                        league_logo = Image.open(BytesIO(requests.get(league_logo_url).content)).convert("RGBA")
+
+                        add_image(league_logo, fig, left=0.12, bottom=0.9, width=0.075, height=0.10)
+                        
+                        # Atak yönü okunu eklemek
+                        ax = fig.add_subplot(111)  # Ana figüre eksen ekleyelim
+                        ax.set_axis_off()  # Eksenleri gizleyelim
+
+                        # FancyArrowPatch ile ok oluşturma
+                        arrow = FancyArrowPatch((0.8075, 0.015), (0.91, 0.015), mutation_scale=20, color=line_color, lw=2.5, arrowstyle='->', transform=ax.transAxes)
+                        ax.add_patch(arrow)  # Oku figüre ekle
+                        
+                        # Atak yönü yazısını eklemek
+                        fig.text(0.788, 0.1, 'Atak Yönü', fontsize=15, ha='center', va='center', fontproperties=prop, color=line_color)
+                        
+                        endnote_ax = plt.axes([0.8, 0.93, 0.1, 0.05])
+                        endnote_ax.text(0.98,0.5,'Veri: WhoScored ve FotMob\n@bariscanyeksin', color=line_color, fontsize=14, ha='right', va='center', fontproperties=prop, transform=endnote_ax.transAxes)
+                        endnote_ax.axis('off')
+                                                
+                        # Görselleştirmeleri göster
+                        st.pyplot(fig)
+                        
+                        # Download button
+                        buf = io.BytesIO()
+                        fig.savefig(buf, format="png", dpi = 300, bbox_inches = "tight")
+                        buf.seek(0)
+                        
+                        st.download_button(
+                            label="Grafiği İndir",
+                            data=buf,
+                            file_name=f"{player_name}-{team_name}-{leagueName}-{weekString}.png",
+                            mime="image/png"
+                        )
+                    
+                    # Görselleştirmeleri oluştur
+                    generate_visualizations()
                 else:
-                    st.info("Lütfen bir oyuncu seçin.")
-        else:
-            st.error("Failed to fetch match data. Please check the match ID and try again.")
+                    st.error("Seçilen oyuncunun bilgileri alınamadı. Lütfen tekrar deneyin.")
+            else:
+                st.info("Lütfen bir oyuncu seçin.")
     else:
         st.error("Failed to fetch Fotmob data. Please check the IDs and try again.")
+
 
